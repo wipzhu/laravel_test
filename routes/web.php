5d31f27c461593ba788df6e2fa9f1843e180bb5b
wipzhu <wipzhu@qq.com>
2019-05-09 15:38:55 +0800

    登录页

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 登录模块
+Route::match(['get', 'post'], '/admin/login', 'Admin\ManagerController@login');
